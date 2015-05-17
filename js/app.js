fb64240d0cdc0e677d3a505b1482cae65aee93ab
Jeremy Shanks <jeremy@shanksenterprise.com>
2015-05-17 17:53:09 -0400

    Working MVP

--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = x;
+    this.y = y;
 }
 
 // Update the enemy's position, required method for game
@@ -14,6 +16,14 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    if (this.x < 505) {
+        this.x += (150 * dt)
+    }
+    else {this.x = -90}
+
+    if(this.x < player.x + 30 && this.x + 60 > player.x && this.y < player.y + 60 && this.y + 40 > player.y) {
+        player.reset();
+    }
 }
 
 // Draw the enemy on the screen, required method for game
@@ -25,11 +35,53 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
+var Player = function () {
+    this.sprite = 'images/char-boy.png';
+    this.x = 200;
+    this.y = 320;
+}
+
+Player.prototype.update = function() {
+ if (player.y < 20)
+    player.reset();
+}
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(direction) {
+    if(direction == 'left' && this.x > 0) {
+        this.x -= 50;
+    }
+    if(direction == 'right' && this.x < 400) {
+        this.x += 50;
+    }
+    if(direction == 'up' && this.y > 3) {
+        this.y -= 50;
+    }
+    if(direction == 'down' && this.y < 400) {
+        this.y += 50;
+    }
+}
+
+Player.prototype.reset = function() {
+    this.x = 200;
+    this.y = 320;
+}
 
 // Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+var enemy1 = new Enemy(-90, 60);
+var enemy2 = new Enemy(-190, 140);
+var enemy3 = new Enemy(-290, 230);
+var enemy4 = new Enemy(-390, 140);
+var enemy5 = new Enemy(-490, 60);
+var enemy6 = new Enemy(-890, 230);
 
+// Place all enemy objects in an array called allEnemies
+var allEnemies = [enemy1, enemy2, enemy3, enemy4, enemy5, enemy6];
+// Place the player object in a variable called player
+var player = new Player;
 
 
 // This listens for key presses and sends the keys to your
