0c9cfb14e42a61dc10c53ae97160f9e2cfd2ea9c
Jeremy Shanks <jshanks24@gmail.com>
2015-05-19 06:29:36 -0400

    Modified README

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ var Enemy = function(x, y) {
     this.sprite = 'images/enemy-bug.png';
     this.x = x;
     this.y = y;
-}
+};
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -17,19 +17,19 @@ Enemy.prototype.update = function(dt) {
     // which will ensure the game runs at the same speed for
     // all computers.
     if (this.x < 505) {
-        this.x += (150 * dt)
+        this.x += (150 * dt);
     }
-    else {this.x = -90}
+    else {this.x = -90;}
 
     if(this.x < player.x + 30 && this.x + 60 > player.x && this.y < player.y + 60 && this.y + 40 > player.y) {
         player.reset();
     }
-}
+};
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+};
 
 // Now write your own player class
 // This class requires an update(), render() and
@@ -39,12 +39,15 @@ var Player = function () {
     this.sprite = 'images/char-boy.png';
     this.x = 200;
     this.y = 320;
-}
+};
 
 Player.prototype.update = function() {
- if (player.y < 20)
+ if (player.y < 20) {
+    player.score++;
+    console.log(player.score);
     player.reset();
 }
+};
 
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
@@ -63,12 +66,12 @@ Player.prototype.handleInput = function(direction) {
     if(direction == 'down' && this.y < 400) {
         this.y += 50;
     }
-}
+};
 
 Player.prototype.reset = function() {
     this.x = 200;
     this.y = 320;
-}
+};
 
 // Now instantiate your objects.
 var enemy1 = new Enemy(-90, 60);
@@ -81,7 +84,9 @@ var enemy6 = new Enemy(-890, 230);
 // Place all enemy objects in an array called allEnemies
 var allEnemies = [enemy1, enemy2, enemy3, enemy4, enemy5, enemy6];
 // Place the player object in a variable called player
-var player = new Player;
+var player = new Player();
+player.score = 0;
+
 
 
 // This listens for key presses and sends the keys to your
